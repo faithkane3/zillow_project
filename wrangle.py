6392712ca50331ee46c4ad4ec9f9da930b062aa2
Faith Kane <faith.x.kane@gmail.com>
2020-03-18 14:57:19 -0500

    Commit acquire and prep exercises

---
+++ wrangle.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+from env import host, user, password
+
+def get_db_url(db_name):
+    return f"mysql+pymysql://{user}:{password}@{host}/{db_name}"
+
+
+def get_data_from_sql():
+    query = """
+    SELECT customer_id, monthly_charges, tenure, total_charges
+    FROM customers
+    WHERE contract_type_id = 3;
+    """
+    df = pd.read_sql(query, get_db_url('telco_churn'))
+    return df
