e191e2bfbeaf3b8f8ae751d74664e858d4a32cbb
Faith Kane <faith.x.kane@gmail.com>
2020-03-19 14:20:45 -0500

    Commit complete acquire and prep and wrangle file

--- wrangle.py
+++ wrangle.py
@@ -15,3 +15,10 @@ def get_data_from_sql():
     """
     df = pd.read_sql(query, get_db_url('telco_churn'))
     return df
+
+def wrangle_telco():
+    df = get_data_from_sql()
+    df.tenure.replace(0, 1, inplace=True)
+    df.total_charges.replace(' ', df.monthly_charges, inplace=True)
+    df.total_charges = df.total_charges.astype(float)
+    return df
