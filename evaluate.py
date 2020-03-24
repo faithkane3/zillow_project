846806f7335b76adecf3e7535a6a314185005567
Faith Kane <faith.x.kane@gmail.com>
2020-03-23 22:45:10 -0500

    Commit exercise complete with functions in notebook and .py file

--- evaluate.py
+++ evaluate.py
@@ -9,16 +9,21 @@ from sklearn.feature_selection import f_regression
 from math import sqrt
 import matplotlib.pyplot as plt
 
-def plot_residuals(X, y):
+def plot_residuals(x, y, df):
     """
     Takes: 
-           X, vector or string
-           y, vector or string
+           x, feature string
+           y, target string
+           df
     Returns: 
-           a residual plot of line regression
-           of y on X
+           plot of the residuals from linear regression
+           of y on x
     """
-    return sns.residplot(X, y)
+    sns.residplot(x, y, df)
+    plt.title('Residuals')
+    plt.xlabel(x)
+    plt.ylabel(y)
+    plt.show()
 
 
 def regression_errors(y, yhat):
@@ -35,6 +40,11 @@ def regression_errors(y, yhat):
     ESS = sum((yhat - y.mean())**2)
     TSS = SSE + ESS
     RMSE = sqrt(MSE)
+    print(f'SSE: {round(SSE, 3)}')
+    print(f'ESS: {round(ESS, 3)}')
+    print(f'TSS: {round(TSS, 3)}')
+    print(f'MSE: {round(MSE, 3)}')
+    print(f'RMSE: {round(RMSE,3)}')
     return SSE, ESS, TSS, MSE, RMSE
 
 
@@ -45,10 +55,12 @@ def baseline_mean_errors(y):
     Returns:
           SSE, MSE, RMSE for baseline values
     """
-    from math import sqrt
     bSSE = ((y.mean() - y) ** 2).sum()
     bMSE = bSSE / len(y)
-    bRMSE = sqrt(bMSE)
+    bRMSE = bMSE ** .5
+    print(f'baseline SSE: {round(bSSE, 3)}')
+    print(f'baseline MSE: {round(bMSE, 3)}')
+    print(f'baseline RMSE: {round(bRMSE,3)}')
     return bSSE, bMSE, bRMSE
 
 
@@ -71,9 +83,11 @@ def model_significance(ols_model):
     """Takes:
              ols model
        Returns:
-             R2
+             r2
              p_value
     """
     p_value = ols_model.f_pvalue
-    R2 = ols_model.rsquared
-    return R2, p_value
+    r2 = ols_model.rsquared
+    print(f'The independent variable explains {r2:.2%} of the variance in our dependent variable.')
+    print(f'Our p_value is {round(p_value,4)}')
+    return r2, p_value
