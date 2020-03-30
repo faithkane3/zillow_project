e5b0592a9869a00d75d593786b9944f71588cd57
Faith Kane <faith.x.kane@gmail.com>
2020-03-30 15:12:45 -0500

    Commit comment code to module

--- wrangle.py
+++ wrangle.py
@@ -3,9 +3,12 @@ import numpy as np
 
 from env import host, user, password
 
+# function to get the url
+
 def get_db_url(db_name):
     return f"mysql+pymysql://{user}:{password}@{host}/{db_name}"
 
+# function that passes my query and my url to return df
 
 def get_data_from_sql():
     query = """
@@ -15,6 +18,8 @@ def get_data_from_sql():
     """
     df = pd.read_sql(query, get_db_url('telco_churn'))
     return df
+
+# function that rules them all by acquiring and prepping my df for exploration or modeling
 
 def wrangle_telco():
     """
@@ -27,3 +32,20 @@ def wrangle_telco():
     df.total_charges.replace(' ', df.monthly_charges, inplace=True)
     df.total_charges = df.total_charges.astype(float)
     return df
+
+def wrangle_employees():
+    """
+    Queries the employees database
+    Returns a clean df with four columns:
+    customer_id(object), monthly_charges(float), tenure(int), total_charges(float)
+    """
+    query = """
+    SELECT emp_no, salary, DATEDIFF('2002-09-30', e.hire_date) as datediff, e.hire_date, t.title
+    FROM salaries as s
+    JOIN employees as e USING(emp_no)
+    JOIN titles as t USING(emp_no)
+    WHERE
+    s.to_date > '2002-09-30'
+    AND t.to_date > '2002-09-30';
+    """
+    df = pd.read_sql(query, get_db_url('employees'))
